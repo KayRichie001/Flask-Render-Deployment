b412b87c9174d58253acc8078c9bb7a48e1c5445
unknown <richardkeith233@gmail.com>
2024-09-04 16:45:47 +0300

    Changed wow message

--- my-flask-app.py
+++ my-flask-app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello There Tryin to deploy here</h1>"
+    return "<h1>Hello There Tryin to deploy here WOW what a success!!!!!!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
